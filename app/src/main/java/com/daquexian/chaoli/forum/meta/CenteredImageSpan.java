b2708fd78a38909865c378ad2ba1b6c40d1c65d9
daquexian <daquexian566@gmail.com>
2017-02-07 16:58:10 +0800

    fix bug that CenteredImageSpan doesn't show completely sometimes

--- app/src/main/java/com/daquexian/chaoli/forum/meta/CenteredImageSpan.java
+++ app/src/main/java/com/daquexian/chaoli/forum/meta/CenteredImageSpan.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.text.style.ImageSpan;
@@ -80,5 +81,24 @@ public class CenteredImageSpan extends ImageSpan
 		canvas.translate(x, transY);
 		d.draw(canvas);
 		canvas.restore();
+	}
+
+	public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
+		Drawable d = getDrawable();
+		Rect rect = d.getBounds();
+		if (fm != null) {
+			Paint.FontMetricsInt fmPaint=paint.getFontMetricsInt();
+			int fontHeight = fmPaint.bottom - fmPaint.top;
+			int drHeight=rect.bottom-rect.top;
+			int top= drHeight/2 - fontHeight/4;
+			int bottom=drHeight/2 + fontHeight/4;
+
+			fm.ascent=-bottom;
+			fm.top=-bottom;
+			fm.bottom=top;
+			fm.descent=top;
+		}
+
+		return rect.right;
 	}
 }
